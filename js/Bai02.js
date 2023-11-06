634b09c07a7cad9aecfd260dfac8909a9edd4f0e
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-11-06 18:22:10 +0700

    Update Bai02

--- js/Bai02.js
+++ js/Bai02.js
@@ -1,61 +1,60 @@
-const decimalInput = document.getElementById('decimal-input');
-const binaryInput = document.getElementById('binary-input');
-const resultBinaryInput = document.getElementById('binary-result');
-const resultDecimalInput = document.getElementById('decimal-result');
+const decimalInput = document.getElementById('so-decimal1');
+const binaryInput = document.getElementById('so-binary2');
+const resultBinaryInput = document.getElementById('so-binary1');
+const resultDecimalInput = document.getElementById('so-decimal2');
 const buttonDecToBin = document.getElementById('btnDecToBin');
 const buttonBinToDec = document.getElementById('btnBinToDec');
-const invalidFeedback = document.querySelector('.invalid-feedback');
+const invalidFeedbacks = document.querySelectorAll('.invalid-feedback');
 
-function reset() {
+function reset1() {
     decimalInput.classList.remove('is-invalid');
+    resultBinaryInput.value = '';
+}
+
+function reset2() {
     binaryInput.classList.remove('is-invalid');
-    resultBinaryInput.value='';
     resultDecimalInput.value = '';
 }
 
-decimalInput.addEventListener('focus', reset);
-binaryInput.addEventListener('focus', reset);
+decimalInput.addEventListener('focus', reset1);
+binaryInput.addEventListener('focus', reset2);
 
 buttonDecToBin.addEventListener('click', () => {
     const value = decimalInput.value.trim();
 
     if (!value) {
         decimalInput.classList.add('is-invalid');
-        invalidFeedback.innerText = 'Chuỗi không được để trống';
+        invalidFeedbacks[0].innerText = 'Chuỗi không được để trống';
         return;
     }
     const regexSoNguyenDuong = /^[1-9]\d*$/;
     if (!regexSoNguyenDuong.test(value)) {
         decimalInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
+        invalidFeedbacks[0].innerText =
             'Chuỗi được nhập phải là số nguyên dương';
         return;
     }
     const regexSoNguyen = /^-?\d+$/;
     if (!regexSoNguyen.test(value)) {
         decimalInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
-            'Chuỗi được nhập phải là số nguyên';
+        invalidFeedbacks[0].innerText = 'Chuỗi được nhập phải là số nguyên';
         return;
     }
     if (!regexSoNguyen.test(value)) {
         decimalInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
-            'Chuỗi được nhập phải là số nguyên';
+        invalidFeedbacks[0].innerText = 'Chuỗi được nhập phải là số nguyên';
         return;
     }
-    const regexSo =/^\d+$/;
+    const regexSo = /^\d+$/;
     if (!regexSo.test(value)) {
         decimalInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
-            'Chuỗi được nhập phải là số';
+        invalidFeedback.innerText = 'Chuỗi được nhập phải là số';
         return;
     }
-    const regexDoDaiChuoi =/^.{1,64}$/;
+    const regexDoDaiChuoi = /^.{1,64}$/;
     if (!regexDoDaiChuoi.test(value)) {
         decimalInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
-            'Chuỗi không được quá 64 ký tự';
+        invalidFeedback.innerText = 'Chuỗi không được quá 64 ký tự';
         return;
     }
 
@@ -69,21 +68,20 @@ buttonBinToDec.addEventListener('click', () => {
 
     if (!value) {
         binaryInput.classList.add('is-invalid');
-        invalidFeedback.innerText = 'Chuỗi không được để trống';
+        invalidFeedbacks[1].innerText = 'Chuỗi không được để trống';
         return;
     }
     const regexBin = /^[01]+$/;
     if (!regexBin.test(value)) {
         binaryInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
+        invalidFeedbacks[1].innerText =
             'Chuỗi được nhập phải là chuỗi nhị phân';
         return;
     }
-    const regexDoDaiChuoi =/^.{1,64}$/;
+    const regexDoDaiChuoi = /^.{1,64}$/;
     if (!regexDoDaiChuoi.test(value)) {
         binaryInput.classList.add('is-invalid');
-        invalidFeedback.innerText =
-            'Chuỗi không được quá 64 ký tự';
+        invalidFeedbacks[1].innerText = 'Chuỗi không được quá 64 ký tự';
         return;
     }
 
@@ -94,27 +92,27 @@ buttonBinToDec.addEventListener('click', () => {
 
 function DecToBin(dec) {
     if (dec === 0) {
-      return "0"; // Trường hợp đặc biệt khi số thập phân là 0.
+        return '0'; // Trường hợp đặc biệt khi số thập phân là 0.
     }
-  
-    var bin = "";
+
+    var bin = '';
     while (dec > 0) {
-      var soDu = dec % 2;
-      bin = soDu + bin;
-      dec = Math.floor(dec / 2);
+        var soDu = dec % 2;
+        bin = soDu + bin;
+        dec = Math.floor(dec / 2);
     }
-  
+
     return bin;
 }
 
 function BinToDec(bin) {
     var dec = 0;
     var doDai = bin.length;
-  
+
     for (var i = 0; i < doDai; i++) {
-      var bit = parseInt(bin.charAt(i));
-      dec += bit * Math.pow(2, doDai - 1 - i);
+        var bit = parseInt(bin.charAt(i));
+        dec += bit * Math.pow(2, doDai - 1 - i);
     }
-  
+
     return dec;
 }
