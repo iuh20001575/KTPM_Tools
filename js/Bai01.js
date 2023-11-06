75eb4d27adcc7634fe81a97ff4d51afabc909331
NgocLinh3110 <85429214+NgocLinh3110@users.noreply.github.com>
2023-11-06 17:34:01 +0700

    Fix Bai01 js

--- js/Bai01.js
+++ js/Bai01.js
@@ -1,14 +1,14 @@
-const tinhBtn = document.querySelector('button-tinh');
-const dongBtn = document.querySelector('button-dong');
+const tinhBtn = document.getElementById('button-tinh');
+const dongBtn = document.getElementById('button-dong');;
 const soCuInput = document.querySelector('.so-cu-txt');
 const soMoiInput = document.querySelector('.so-moi-txt');
 const invalidFeedback = document.querySelector('.invalid-feedback');
 const thanhTienInput = document.getElementById('thanh-tien');
 
 function reset() {
-    soCuInput.classList.remove('is-invalid');
-    soMoiInput.classList.remove('is-invalid');
-    thanhTienInput.value = '';
+  soCuInput.classList.remove('is-invalid');
+  soMoiInput.classList.remove('is-invalid');
+  thanhTienInput.value = '';
 }
 
 soCuInput.addEventListener('focus', reset);
@@ -47,36 +47,34 @@ tinhBtn.addEventListener('click', () => {
 });
 
 dongBtn.addEventListener('click', () => {
-    soCuInput.value = " ";
-    soMoiInput.value = " ";
-})
+  soCuInput.value = "";
+  soMoiInput.value = "";
+});
 
 function tinhThanhTien(soCu, soMoi) {
-
-    var thanhTien = 0;
-    const bac = soMoi-soCu;
-    switch (bac) {
-      case (bac>=0&&bac<=50):
-        thanhTien = bac * 1484 + 0.1 * bac * 1484;
-        break;
-      case (bac>=51&&bac<=100):
-        thanhTien = (50 * 1484 + (bac-50)*1533) + 0.1 * (50 * 1484 + (bac-50)*1533);
-        break;
-      case (bac>=101&&bac<=200):
-        thanhTien = (50 * 1484 + 50*1533 +(bac-100)*1786) + 0.1 * (50 * 1484 + 50*1533 +(bac-100)*1786);
-        break;
-      case (bac>=201&&bac<=300):
-        thanhTien = (50 * 1484 + 50*1533 +100*1786+(bac-200)*2242) + 0.1 *(50 * 1484 + 50*1533 +100*1786+(bac-200)*2242);
-        break;
-      case (bac>=301&&bac<=400):
-        thanhTien = (50 * 1484 + 50*1533 +100*1786+100*2242+(bac-300)*2503) + 0.1 * (50 * 1484 + 50*1533 +100*1786+100*2242+(bac-300)*2503);
-        break;
-      case (bac>=401):
-        thanhTien = (50 * 1484 + 50*1533 +100*1786+100*2242+100*2503+(bac-400)*2587) + 0.1 * (50 * 1484 + 50*1533 +100*1786+100*2242+100*2503+(bac-400)*2587);
-        break;
+  var thanhTien = 0;
+  const bac = soMoi - soCu;
+  switch (true) {
+      case bac >= 0 && bac <= 50:
+          thanhTien = bac * 1484 + 0.1 * bac * 1484;
+          break;
+      case bac >= 51 && bac <= 100:
+          thanhTien = (50 * 1484 + (bac - 50) * 1533) + 0.1 * (50 * 1484 + (bac - 50) * 1533);
+          break;
+      case bac >= 101 && bac <= 200:
+          thanhTien = (50 * 1484 + 50 * 1533 + (bac - 100) * 1786) + 0.1 * (50 * 1484 + 50 * 1533 + (bac - 100) * 1786);
+          break;
+      case bac >= 201 && bac <= 300:
+          thanhTien = (50 * 1484 + 50 * 1533 + 100 * 1786 + (bac - 200) * 2242) + 0.1 * (50 * 1484 + 50 * 1533 + 100 * 1786 + (bac - 200) * 2242);
+          break;
+      case bac >= 301 && bac <= 400:
+          thanhTien = (50 * 1484 + 50 * 1533 + 100 * 1786 + 100 * 2242 + (bac - 300) * 2503) + 0.1 * (50 * 1484 + 50 * 1533 + 100 * 1786 + 100 * 2242 + (bac - 300) * 2503);
+          break;
+      case bac >= 401:
+          thanhTien = (50 * 1484 + 50 * 1533 + 100 * 1786 + 100 * 2242 + 100 * 2503 + (bac - 400) * 2587) + 0.1 * (50 * 1484 + 50 * 1533 + 100 * 1786 + 100 * 2242 + 100 * 2503 + (bac - 400) * 2587);
+          break;
       default:
-        return -1;
-    }
-  
-    return thanhTien;
+          return -1;
   }
+  return thanhTien;
+}
